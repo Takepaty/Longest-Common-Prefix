10431c58a46a3c60b10c8364d239c70d94785213
Patricia Takehara <paty.takeharab@outlook.com>
2023-02-03 15:35:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int minLength = strs.Min(e => e.Length);
+        string res = strs.First(e => e.Length == minLength);
+
+        foreach (string str in strs)
+        {
+            for (int j = 0; j < minLength; j++)
+            {
+                if (str[j] != res[j])
+                {
+                    minLength = j; break;                   
+                }
+            }
+        }
+        return res.Substring(0, minLength);
+
+    }
+}
